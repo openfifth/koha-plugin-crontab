6f3d495a066c55a0d1bb14d4fb2d6888f24a80b8
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-02-10 07:33:11 +0000

    chore: bump version

--- Koha/Plugin/Com/OpenFifth/Crontab.pm
+++ Koha/Plugin/Com/OpenFifth/Crontab.pm
@@ -35,7 +35,7 @@ use JSON;
 
 use C4::Context;
 
-our $VERSION         = '1.3.5';
+our $VERSION         = '1.3.6';
 our $MINIMUM_VERSION = "22.11.00.000";
 
 our $metadata = {
@@ -43,7 +43,7 @@ our $metadata = {
     author          => 'Martin Renvoize',
     description     => 'Script scheduling',
     date_authored   => '2023-04-25',
-    date_updated    => '2026-02-09',
+    date_updated    => '2026-02-10',
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
